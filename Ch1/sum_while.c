646631eb9c92b033202e8223e4b69e4bd26b4075
EOM TAE HYUN <sku0807@naver.com>
2020-11-23 15:02:22 +0900

    sum_while.c

---
+++ Ch1/sum_while.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n;
+	int sum;
+	puts("1부터 n까지의 합을 구합니다.");
+	printf("n의 값: ");
+	scanf("%d", &n);
+	sum = 0;
+	i = 1;
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+
+	return 0;
+}
